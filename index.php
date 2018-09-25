e1645d1786965a97dde173b2f4c41c1233255076
Kresimir Meze <kresimir.meze@gmail.com>
2018-09-25 14:23:03 +0200

    Added WordPress Loop.
    If WordPress cannot find any matching template file, the theme’s index.php file will be used. This template will by default display title and content of found posts.

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php 
     get_header();
+    
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_title('<h1>', '</h1>');
+        the_content();
+    endwhile; else :
+        _e( 'Sorry, no content found.', 'pressite' );
+    endif;
+
     get_footer();
